4cb465e77a4a1ddbcd34986526cc5c4871755587
Malasia Woods <malasiawoods@Malasias-Air.hsd1.ca.comcast.net>
2020-08-18 17:37:22 -0700

    test passing

--- test/questions.spec.js
+++ test/questions.spec.js
@@ -1,7 +1,7 @@
 const QuestionsService = require('../src/questions/questions-service')
 const knex = require('knex')
 
-   describe(`getAllQuestions()`, () => {
+  //  describe(`getAllQuestions()`, () => {
     let db
     let testQuestions = [
         {
@@ -29,19 +29,39 @@ const knex = require('knex')
       //  before('cleanup', () => db.raw('TRUNCATE TABLE answers;'));
        before('cleanup', () => db.raw('TRUNCATE TABLE questions CASCADE;'));
 
-       before(() => {
-           return db
-             .into('questions')
-             .insert(testQuestions)
-         })
+       afterEach(() => db.raw('TRUNCATE TABLE questions CASCADE;'))
+
+      //  before(() => {
+      //      return db
+      //        .into('questions')
+      //        .insert(testQuestions)
+      //    })
 
          after(() => db.destroy())
 
-     it(`resolves all questions from questions table`, () => {
+    describe(`getAllQuestions()`, () => {
+      context(`Given 'questions' has data`, () => {
+           before(() => {
+             return db
+               .into('questions')
+               .insert(testQuestions)
+           })
+
+      it(`getAllQuestions() resolves all questions from 'questions' table`, () => {
        // test that ArticlesService.getAllArticles gets data from table
        return QuestionsService.getAllQuestions(db)
        .then(actual => {
-         expect(actual).to.eql(testQuestions)
+        expect(actual).to.eql(testQuestions)
        })
      })
+  })
+  context(`Given 'questions' has no data`, () => {
+       it(`getAllQuestions() resolves an empty array`, () => {
+         return QuestionsService.getAllQuestions(db)
+           .then(actual => {
+             expect(actual).to.eql([])
+           })
+       })
+     })
+
 }) 

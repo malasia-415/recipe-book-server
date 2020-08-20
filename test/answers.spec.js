0012bc7a7ed0d3dc7a3ab78ff909a6fce336a9fb
Malasia Woods <malasiawoods@Malasias-Air.hsd1.ca.comcast.net>
2020-08-20 15:09:13 -0700

    Fianl changes for test fliles now test working

--- test/answers.spec.js
+++ test/answers.spec.js
@@ -1,83 +1,111 @@
-const AnswersService = require('../src/answers/answers-service')
+const AnswerService = require('../src/answers/answer-service')
 const knex = require('knex')
-
-//  describe(`getAllAnswers()`, () => {
-let db
-let testAnswers = [
-    {
-        title: 'First answers',
-        description: 'This is your first answers',
-        author: 'Jone',
-        id: 1
-    },
-
-    {
-        title: 'Second answer',
-        description: 'This is your second answer',
-        author: 'Ashley',
-        id: 2
-    }
-]
-
-    before(() => {
-        db = knex({
-          client: 'pg',
-          connection: process.env.TEST_DATABASE_URL,
-        })
-      })
-
-      //  before('cleanup', () => db.raw('TRUNCATE TABLE answers;'));
+    let db
+    let testAnswers = [
+        {
+            title: 'First answer',
+            description: 'This is your first answer',
+            author: 'Jone',
+            id: 1
+        },
+        {
+            title: 'Second answer',
+            description: 'This is your second answer',
+            author: 'Ashley',
+            id: 2
+        }
+    ]
+       before(() => {
+         db = knex({
+           client: 'pg',
+           connection: process.env.TEST_DATABASE_URL,
+         })
+       })
       before('cleanup', () => db.raw('TRUNCATE TABLE answers CASCADE;'));
+      afterEach('cleanup', () => db.raw('TRUNCATE TABLE answers CASCADE;'))
+      
+      after('destroy db connection', () => db.destroy())
+      
+  describe('getAllAnswers()', () => {
+    it('returns an empty array', () => {
+      return AnswerService
+        .getAllAnswers(db)
+        .then(answers => expect(answers).to.eql([])
+        );
+    }); 
+ });
 
-      afterEach('cleanup', () => db.raw('TRUNCATE TABLE answers CASCADE;'));
 
-      after(() => db.destroy())
 
-      describe(`getAllAnswers()`, () => {
-        context(`Given 'answers' has data`, () => {
-             beforeEach(() => {
-               return db
-                 .into('answers')
-                // .getAllAnswers(db)
-                .insert(testAnswers)
-                
-             })
 
-             it(`getAllAnswers() resolves all answers from 'answers' table`, () => {
-                // test that ArticlesService.getAllArticles gets data from table
-                return AnswersService.getAllAnswers(db)
-                .then(actual => {
-                 expect(actual).to.eql(testAnswers)
-                })
-              })
-           })   
 
-           context(`Given 'answers' has no data`, () => {
-            it(`getAllAnswers() resolves an empty array`, () => {
-              return AnswersService.getAllAnswers(db)
-                .then(actual => {
-                  expect(actual).to.eql([])
-                })
-            })
-          })
-          it(`insertAnswers() inserts a new answers and resolves the new answers with an 'id'`, () => {
-            const newAnswer = {
-              title: 'First answer',
-              description: 'This is your first answer',
-              author: 'Jone',
-            }
 
-            getAnswersById(knex, id) 
-            return knex.from('answers').select('*').where('id', id).first()   
+  describe('insertAnswers()' , () => {
+    it('inserts record in db and returns answer with new id', () => {
 
-            return AnswersService.insertAnswer(db, newAnswer)
-            .then(actual => {
-              expect(actual).to.eql({
-                id: 1,
-                title: newAnswer.title,
-                author: newAnswer.author,
-                description: newAnswer.description
-              });
-            });
-        }) 
-      })   
+      const newAnswer = {
+        id: 1,
+        answer: 'First answer',
+        description: 'This is your first answer',
+        author: 'Jone',
+    };
+
+    it('throws not-null constraint error if title not provided', () => {    
+      const newAnswer = {
+        id: 1,
+        answer: 'New test',
+        description: 'Test new description',
+        author: 'James'
+      };
+
+      return AnswerService.insertAnswer(db, newAnswer)
+        .then(actual => {
+          expect(actual).to.eql({
+            id: 1,
+            answer: newAnswer.answer,
+            description: newAnswer.description,
+            author: newAnswer.author,
+          });
+        });
+      });
+    
+   });
+});
+
+
+
+
+describe('getAnswersById()', () => {
+
+    context('with data present', () => {
+        before('answers', () => 
+         db('answers')
+        .insert(testAnswers)
+     );
+  });
+});
+
+
+
+      
+describe('deleteAnswer()', () => {
+
+    context('with data present', () => {
+       before('insert answer', () => 
+          db('answers')
+         .insert(testAnswers)
+    );
+  });
+});
+    
+
+
+describe('updateAnswer()', () => {
+
+    context('with data present', () => {
+       before('answers', () => 
+        db('answers')
+        .insert(testAnswers)
+    );
+  });
+});
--- test/questions.spec.js
+++ test/questions.spec.js
@@ -1,9 +1,6 @@
-const knex = require('knex');
-const QuestionsService = require('../src/questions/questions-service');
-
-   describe(`getAllQuestions()`, () => {
-    let db;
-    
+const QuestionsService = require('../src/questions/questions-service')
+const knex = require('knex')
+    let db
     let testQuestions = [
         {
             title: 'First question',
@@ -11,7 +8,6 @@ const QuestionsService = require('../src/questions/questions-service');
             author: 'Jone',
             id: 1
         },
-
         {
             title: 'Second question',
             description: 'This is your second question',
@@ -19,123 +15,71 @@ const QuestionsService = require('../src/questions/questions-service');
             id: 2
         }
     ]
-    
        before(() => {
          db = knex({
            client: 'pg',
            connection: process.env.TEST_DATABASE_URL,
          })
        })
+      before('cleanup', () => db.raw('TRUNCATE TABLE questions CASCADE;'));
+      afterEach('cleanup', () => db.raw('TRUNCATE TABLE questions CASCADE;'))
+      
+      after('destroy',() => db.destroy())
 
-      //  before('cleanup', () => db.raw('TRUNCATE TABLE answers;'));
-       before('cleanup', () => db.raw('TRUNCATE TABLE questions CASCADE;'));
+  describe('getAllQuestions()', () => {
+    it('returns an empty array', () => {
 
-       afterEach('cleanup', () => db.raw('TRUNCATE TABLE questions CASCADE;'));
-
-      //  before(() => {
-      //      return db
-      //        .into('questions')
-      //        .insert(testQuestions)
-      //    })
-
-         after(() => db.destroy())
-
-    describe(`getAllQuestions()`, () => {
-      context(`Given 'questions' has data`, () => {
-           beforeEach(() => {
-             return db
-               .into('questions')
-              // .getAllQuestions(db)
-              .insert(testQuestions)
-              
-           })
-
-      it(`getAllQuestions() resolves all questions from 'questions' table`, () => {
-       // test that ArticlesService.getAllArticles gets data from table
-       return QuestionsService.getAllQuestions(db)
-       .then(actual => {
-        expect(actual).to.eql(testQuestions)
-       })
-     })
-  })
-  context(`Given 'questions' has no data`, () => {
-       it(`getAllQuestions() resolves an empty array`, () => {
-         return QuestionsService.getAllQuestions(db)
-           .then(actual => {
-             expect(actual).to.eql([])
-           })
-       })
-     })
-
+      return QuestionsService
+        .getAllQuestions(db)
+        .then(questions => {
+          expect(questions).to.eql([])
+        });
+    });
+    context('with data present', () => {
+      beforeEach('insert test articles', () =>
+        db('questions')
+          .insert(testQuestions)
+      );
+      it('returns all test questions', () => {
+        return QuestionsService
+          .getAllQuestions(db)
+          .then(questions => expect(questions).to.eql(testQuestions));
+      });
+    });
+  });
+  describe('insertQuestions()' , () => {
     
-     it(`insertQuestions() inserts a new questions and resolves the new questions with an 'id'`, () => {
+    it('inserts record in db and returns question with new id', () => {
       const newQuestion = {
+        id: 1,
         title: 'First question',
         description: 'This is your first question',
         author: 'Jone',
       }
 
-      return QuestionsService
-      .updateQuestions(db, updatedQuestionsId, updatedQuestionsId)
-      .insertQuestion(db, newQuestion)
-      .then(actual => {
-        expect(actual).to.eql({
-          id: 1,
-          title: newQuestion.title,
-          author: newQuestion.author,
-          description: newQuestion.description
+      return QuestionsService.insertQuestion(db, newQuestion)
+        .then(actual => {
+          // console.log({actual})
+          expect(actual).to.eql({
+            id: 1,
+            title: newQuestion.title,
+            description: newQuestion.description,
+            author: newQuestion.author,
+          });
         });
-        return db('questions').select('*').where({ id: updatedQuestionsId}).first();
-      });
-  })
-
-getQuestionsById(knex, id) 
-describe('getById()', () => {
-  it('should return undefined', () => {
-        return knex.from('questions').select('*').where('id', id).first()
-        .getById(db, 999)
-        .then(questions => expect(questions).to.be.undefined);
     });
 
-    context('with data present', () => {
-      before('insert articles', () => 
-        db('blogful_articles')
-          .into('questions')
-          .insert(testArticles)
-      );
-})
 
-it(`getQuestionsById() resolves an questions by id from 'questions' table`, () => {
-    const thirdId = 3
-    const thirdTestQuestions = testQuestions[thirdId - 1]
-    return QuestionsService.getQuestionsById(db, thirdId)
-      .then(actual => {
-        expect(actual).to.eql({
-          id: thirdId,
-          title: thirdTestQuestions.title,
-          author: thirdTestQuestions.author,
-          description: thirdTestQuestions.description
-        });
-      });
-    })
-
-it(`updateQuestions() updates an question from the 'question' table`, () => {
-        const idOfQuestionsToUpdate = 3
-        const newQuestionsData = {
-          id: 'updated id',
-          title: 'updated title',
-          author: 'updated content',
-          description: 'updated description',
-        }
-        return QuestionsService.updateQuestions(db, idOfQuestionsToUpdate, newQuestionsData)
-          .then(() => QuestionsService.getQuestionsById(db, idOfQuestionsToUpdate))
-          .then(questions => {
-            expect(questions).to.eql({
-              id: idOfQuetsionsToUpdate,
-              ...newQuestionsData,
-            })
-          })
-        })
-    })
-  })
-})
+    it('throws not-null constraint error if title not provided', () => {    
+      const newQuestion = {
+        description: 'Test new description',
+        author: 'James'
+      };
+      return QuestionsService 
+        .insertQuestion(db, newQuestion)
+        .then(
+          () => expect.fail('db should throw error'),
+          err => expect(err.message).to.include('not-null')
+        );
+    });
+  });
